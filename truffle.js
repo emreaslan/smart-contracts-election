db95425bf6d5f3612e501a1a365bc000e4122a70
Emre Sercan Aslan <emre55aslan@gmail.com>
2018-12-24 08:25:31 +0300

    Configured deployment settings to use test networks

--- truffle.js
+++ truffle.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,6 +8,28 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 1,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 3,
+      gas: 4500000
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 4,
+      gas: 6500000
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   }
 };
